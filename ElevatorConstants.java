69edc59e260934d33bb00754adf3a5b5922411c1
turbojax07 <turbojax07@gmail.com>
2025-04-05 20:23:54 -0400

    Removing auto-generated constants

--- ElevatorConstants.java
+++ ElevatorConstants.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.elevator;
 
 import static edu.wpi.first.units.Units.*;
@@ -12,7 +8,6 @@ import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.units.measure.Mass;
 
-/** Add your docs here. */
 public class ElevatorConstants {
     public static final Mass mass = Pounds.of(25.1);
     public static final Distance radius = Meters.of(0.02864789);
